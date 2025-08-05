53b55b0db027059e41e027d36c715b63eba6f1c8
Pablo Henrique Lima dos Santos <136536368+eng-pablo-tec@users.noreply.github.com>
2025-08-05 16:51:03 -0300

    Create words.js

---
+++ words.js
@@ -0,0 +1,20 @@
+const wordList = [
+  "banal", "cesta", "firme", "lento", "gosto", "molar", "puxar", "risco", "verbo", "zelar",
+  "amigo", "barco", "caixa", "dente", "exato", "feira", "girar", "humor", "ideia", "jogar",
+  "leito", "manso", "nuvem", "olhar", "pacto", "queda", "rente", "saber", "tenso", "urgir",
+  "visto", "zebra", "andar", "bando", "chave", "dados", "elite", "fatos", "ganho", "haste",
+  "índio", "janho", "karma", "lápis", "medir", "nível", "opaco", "parar", "quase", "rasgo",
+  "sente", "tenaz", "único", "valer", "wenge", "xampu", "yacht", "zelos", "abade", "bolsa",
+  "ciclo", "doido", "etapa", "fator", "grave", "hobby", "inato", "jovem", "kiwis", "lento",
+  "moeda", "navio", "obeso", "pente", "quica", "ruído", "solar", "tempo", "único", "verde",
+  "xadrez", "yogar", "zumbi", "aceno", "bazar", "carro", "depor", "épico", "fugaz", "gozar",
+  "hiena", "ícone", "judeu", "koala", "lente", "mídia", "nicho", "óxido", "pleno", "quase",
+  "rocha", "salto", "tocar", "ursoa", "vagar", "watch", "xerox", "yucca", "zenir", "anexo",
+  "brisa", "cofre", "dores", "extra", "focal", "gabar", "haste", "idoso", "jarra", "kenya",
+  "livro", "magia", "nuvem", "opala", "pinho", "quico", "repor", "saber", "tigre", "umbro",
+  "vazio", "xaxim", "yummy", "zanga", "aéreo", "bicho", "casco", "dique", "época", "fisco",
+  "gesso", "haste", "ímpar", "jogar", "kioto", "lugar", "marco", "nível", "óvulo", "pacto",
+  "quoto", "ruína", "sorte", "trama", "único", "viúva", "willy", "xaleu", "yogui", "zorra"
+];
+
+const answer = wordList[Math.floor(Math.random() * wordList.length)];
